b219c8737f24f34d87f5f42dead4b192f719ceef
fumie-kuga <neozoic122@gmail.com>
2023-11-23 20:38:44 +0900

    実習問題用プロジェクト作成

---
+++ UJS68_new_com/lab/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <RouterLink to="/">
+        <h1>実習問題</h1>
+      </RouterLink>
+
+      <nav>
+        <RouterLink to="/">3章　実習問題１</RouterLink><br>
+        <RouterLink to="/">4章　実習問題１</RouterLink><br>
+        <RouterLink to="/">5章　実習問題１</RouterLink><br>
+        <RouterLink to="/">6章　実習問題１</RouterLink><br>
+        <RouterLink to="/">7章　実習問題１</RouterLink><br>
+      </nav>
+
+      <RouterLink to="/">
+        <h1>解答例</h1>
+      </RouterLink>
+
+      <nav>
+        <RouterLink to="/">3章　実習問題１</RouterLink><br>
+        <RouterLink to="/">4章　実習問題１</RouterLink><br>
+        <RouterLink to="/">5章　実習問題１</RouterLink><br>
+        <RouterLink to="/">6章　実習問題１</RouterLink><br>
+        <RouterLink to="/">7章　実習問題１</RouterLink><br>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  margin-top: 5px;
+  margin-bottom: 10px;
+  margin-left: 5px;
+  font-size: 12px;
+}
+</style>
---
+++ UJS68_new_com/lab/src/components/TheWelcome.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="home">
+    <h1>
+      Vue.js によるSPA(シングルページアプリケーション)開発
+    </h1>
+    <h2>実習問題＆解答例</h2>
+    <div class="copy_right">All Rights Reserved, Copyright © 株式会社富士通ラーニングメディア</div>
+  </div>
+</template>
+
+<style scoped>
+.home{
+  position: relative;
+	width: 100%;
+	height: 400px;
+}
+.home h1{
+  text-align: center;
+  margin-top: 100px;
+}
+
+.home h2{
+  text-align: center;
+  margin-top: 50px;
+}
+.home .copy_right{
+  font-size: 10px;
+  position: absolute;
+  bottom: 0;
+	left: 50%;
+	transform: translateX(-50%);  
+}
+</style>
