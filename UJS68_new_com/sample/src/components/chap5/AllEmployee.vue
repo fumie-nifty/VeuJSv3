84beea90f9c553197474dc1673b9633490637fe4
fumie-kuga <neozoic122@gmail.com>
2023-11-25 11:52:05 +0900

    サンプルソース5章までソース整形、コメント追加

--- UJS68_new_com/sample/src/components/chap5/AllEmployee.vue
+++ UJS68_new_com/sample/src/components/chap5/AllEmployee.vue
@@ -2,46 +2,58 @@
 <!-- AllEmployee.vue --> 
 
 <script setup>
-import { ref, reactive,onMounted } from 'vue'
-import axios from "axios"
+	import { ref, reactive, onMounted } from 'vue'
 
-const employeeId = ref('')
-const message = ref('従業員番号を入力して検索ボタンをクリックして下さい')
+	//axiosのインポート
+	import axios from "axios"
 
-const employeeList = ref([])
+	// メッセージ
+	const message = ref('')
+	// 検索結果フラグ
+	const searchFlag = ref(false)
 
-onMounted(() => {
+	// 従業員リスト
+	const employeeList = ref([])
+
 	const url = 'http://localhost:3000/employee/'
 
 	axios.get(url)
 		.then((response) => {
-			message.value = '検索に成功しました'
+			//通信結果取得したデータをemplloyeeに格納
 			employeeList.value = response.data
+			message.value = '検索に成功しました'
+			//検索結果のフラグをtureにする
+			searchFlag.value = true
+			// 取得した配列が空だった場合
+			if(employeeList.value.length == 0){
+				message.value = '従業員データが存在しません'
+				//検索結果のフラグをfalseにする
+				searchFlag.value = false
+			}
 		})
 		.catch((error) => {
-			message.value ='検索に失敗しました'
+			message.value = '検索に失敗しました'
+			//検索結果のフラグをfalseにする
+			searchFlag.value = false
 			employeeList.value = []
 			console.log(error)
 		})
-
-})
-
 </script>
 
 <template>
 	<p>従業員一覧</p>
-	<p class="message">{{ message }}</p>
-	<table border="1">
-    <thead>
-      <tr>
-        <th>従業員番号</th>
-        <th>従業員名</th>
-        <th>所属</th>
-        <th>内線</th>
-      </tr>
-    </thead>
+	<p v-bind:class="{ red: !searchFlag }">{{ message }}</p>
+	<table v-show="searchFlag">
+		<thead>
+			<tr>
+				<th>従業員番号</th>
+				<th>従業員名</th>
+				<th>所属</th>
+				<th>内線</th>
+			</tr>
+		</thead>
 		<tbody>
-			<tr v-for="(employee,index) in employeeList" v-bind:key="employee.id">
+			<tr v-for="(employee, index) in employeeList" v-bind:key="employee.id">
 				<td>{{ employee.id }}</td>
 				<td>{{ employee.name }}</td>
 				<td>{{ employee.section }}</td>
@@ -51,4 +63,20 @@ onMounted(() => {
 	</table>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.red {
+		color: red;
+	}
+
+	table {
+		border-collapse: collapse;
+	}
+
+	table,
+	th,
+	td {
+		padding: 0px 10px 0px 10px;
+		border: 1px solid #333;
+	}
+</style>
+
