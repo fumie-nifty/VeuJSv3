147bead3570c504cdaf1dcf8f7a75126da5fc078
fumie-kuga <neozoic122@gmail.com>
2023-12-06 02:49:59 +0900

    コメント修正、json-serverのpackage.jsonにエイリアス追加

--- UJS68_new_com/sample/src/components/chap4/InsertEmployeeAns.vue
+++ UJS68_new_com/sample/src/components/chap4/InsertEmployeeAns.vue
@@ -2,43 +2,42 @@
 <!-- InsertEmployeeAns.vue --> 
 
 <script setup>
-	import { ref, reactive } from 'vue'
+  import { ref, reactive } from 'vue'
 
-	// 従業員名
-	const employeeName = ref('')
+  // 従業員名
+  const employeeName = ref('')
 
-	// 従業員リスト
-	const employeeNameList = reactive([])	// Todo1
+  // 従業員リスト
+  const employeeNameList = reactive([])	// Todo1
 
-	// 従業員追加メソッド
-	const insertMember = () => {
-		// Todo2
-		if (employeeName.value !== '') {
-			employeeNameList.push(employeeName.value)
-			employeeName.value = ''
-		}
-	}
+  // 従業員追加メソッド
+  const insertMember = () => {
+    // Todo2
+    if (employeeName.value !== '') {
+      employeeNameList.push(employeeName.value)
+      employeeName.value = ''
+    }
+  }
 </script>
 
 <template>
-	<p>従業員登録</p>
-	<!-- Todo3 -->
-	<input type="text" v-model="employeeName">
-	<!-- Todo4 -->
-	<button v-on:click="insertMember">登録</button>
-	<table border="1">
-		<thead>
-			<th>No</th>
-			<th>従業員名</th>
-		</thead>
-		<tbody>
-			<tr v-for="(name, index) in employeeNameList" v-bind:key="name">
-				<td>{{ index + 1 }}</td>
-				<td>{{ name }}</td>
-			</tr>
-		</tbody>
-	</table>
+  <p>従業員登録</p>
+  <!-- Todo3 -->
+  <input type="text" v-model="employeeName">
+  <!-- Todo4 -->
+  <button v-on:click="insertMember">登録</button>
+  <table border="1">
+    <thead>
+      <th>No</th>
+      <th>従業員名</th>
+    </thead>
+    <tbody>
+      <tr v-for="(name, index) in employeeNameList" v-bind:key="name">
+        <td>{{ index + 1 }}</td>
+        <td>{{ name }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped></style>
-
